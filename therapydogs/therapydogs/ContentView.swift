52bd5140ef9951f76b49c427def0e21f573b87cd
Eric May <eric.may@boldpenguin.com>
2023-10-14 09:55:43 -0400

    Added therapy dog pictures and initial Xcode project

---
+++ therapydogs/therapydogs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  therapydogs
+//
+//  Created by Eric May on 10/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ therapydogs/therapydogs/therapydogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  therapydogsApp.swift
+//  therapydogs
+//
+//  Created by Eric May on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct therapydogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
